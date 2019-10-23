1710f32c70fbab38b329fcb19a96f746e663cc95
xuhj5 <xuhj5@lenovo.com>
2019-10-23 18:22:24 +0800

    page redirection

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const path = require('path');
 const express = require('express');
 
 const mainRouter = require('./routes/main');
+const homeRouter = require('./routes/home');
 
 const app =new express();
 
 app.use(express.static(path.join(__dirname,'resources'))); // Get Static resources
 app.use(mainRouter);
+app.use(homeRouter);
 
 app.listen(3000);
