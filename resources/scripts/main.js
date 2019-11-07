a49420a7ff16d02b3f2082959393057bf16f335f
xuhj5 <xuhj5@lenovo.com>
2019-11-07 17:49:06 +0800

    use vue pure start

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -1,3 +1,20 @@
-$("#loginButton").on("click",function(){
-  window.location.href="/home";
+$("#loginButton").on("click", function () {
+  window.location.href = "/home";
 });
+
+//define local login component
+var loginComponent={ 
+  template: `<form>
+<input type="text" placeholder="username">
+<input type="password" >
+<input type="button" value="Login" id="loginButton">
+</form>`
+}
+
+new Vue({ 
+  el:"#loginComponent",
+  //local registration components
+  components: {
+    'login-component': loginComponent
+  }
+})
